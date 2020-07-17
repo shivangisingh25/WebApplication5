d98f95f9b7d1e0de2d8005efa7b97a87b495fe99
Shivangi Singh <shivangisingh211@outlook.com>
2020-07-17 13:52:49 +0530

    changed value

--- Controllers/valuesController.cs
+++ Controllers/valuesController.cs
@@ -15,7 +15,7 @@ namespace WebApplication5.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Shivangi", "Singh" };
         }
 
         
